37cacf17f108f41f4c36c3244dac909335216d1b
Konnov Konstantin <konstantin.konnov@panter-dsd.ru>
2020-07-02 16:30:02 +0300

    Create build.cmd

---
+++ build.cmd
@@ -0,0 +1,12 @@
+@echo off
+pushd "%~dp0"
+
+powershell Invoke-WebRequest -o C:\TEMP\conan.exe https://dl.bintray.com/conan/installers/conan-win-32_1_26_0.exe 
+C:\TEMP\conan.exe /VERYSILENT /DIR="c:\conan\"
+c:\conan\conan\conan.exe profile new default --detect
+
+c:\conan\conan\conan.exe remote add bincrafters https://api.bintray.com/conan/bincrafters/public-conan True 
+c:\conan\conan\conan.exe install --build=missing protobuf/3.9.1@bincrafters/stable
+:exit
+popd
+@echo on
